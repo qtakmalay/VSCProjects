c14a2908ca3b118325048ed998324aee575add48
Qtakmalay <azat.v.r.15@gmail.com>
2022-11-29 23:09:34 +0100

    Finished 4th ex

--- 5thUnit/ex4.py
+++ 5thUnit/ex4.py
@@ -1,11 +1,14 @@
-def flatten(nested: list) -> list:
-    fl_list = list()
-    curr_i = 0
-    if not isinstance(nested, list):
+def flatten(nested : list):
+    if not nested:
         return nested
-    for i, val in enumerate(nested):
+    if isinstance(nested[0], list):
+        return flatten(*nested[:1]) + flatten(nested[1:])
+    return nested[:1] + flatten(nested[1:])
+             
 
-
+    
 print(flatten([1, 2, [4, [8, 9, [11, 12], 10], 5], 3, [6, 7]])) #= [1, 2, 4, 8, 9, 11, 12, 10, 5, 3, 6, 7]
 print(flatten([[]])) #= []
 print(flatten([[], [], [1], [], [1, [], [4, 5, [[[6]]]], 2, 3]])) #= [1, 1, 4, 5, 6, 2, 3]
+
+
