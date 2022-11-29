648ca6e6cfd14baeebcf25252d35b59770527208
Qtakmalay <azat.v.r.15@gmail.com>
2022-11-29 23:55:23 +0100

    Finished 5th

--- 5thUnit/ex5.py
+++ 5thUnit/ex5.py
@@ -2,10 +2,10 @@ def f(x: int):
     try:
         g(x)
         print("f1")
-    except ErrorA:
+    except TypeError:
         print("f2")
-        raise ErrorC
-    except ErrorB:
+        raise print(KeyError)
+    except ValueError:
         print("f3")
     else:
         print("f4")
@@ -14,19 +14,27 @@ def g(x: int):
     try:
         h(x)
         print("g1")
-    except ErrorA:
+    except TypeError:
         print("g2")
         if x < -10:
-            raise ErrorC
+            print(KeyError)
+            #raise print(KeyError)
         print("g3")
     finally:
         print("g4")
 def h(x: int):
     try:
         if x < 0:
-            raise ErrorA
+            print(TypeError)
         if x > 10:
-            raise ErrorB
+            print(ValueError)
     finally:
         print("h1")
         print("h2")
+print(f(1))
+print("----------------------")
+print(f(-1))
+print("----------------------")
+print(f(15))
+print("----------------------")
+print(f(-15))
