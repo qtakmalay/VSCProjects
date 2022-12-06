bf96d7ef6ade049bd47f6a5cafc3bedd1749b094
Qtakmalay <azat.v.r.15@gmail.com>
2022-12-06 23:34:53 +0100

    ex1 finished

--- 6thUnit/ex1.py
+++ 6thUnit/ex1.py
@@ -2,18 +2,17 @@ import os, re
 def read_numbers(path: str) -> list:
     ab_path = os.path.dirname(__file__)
     fl_reader = open(os.path.join(ab_path, path), "r")
-    fl_numsl = list(str(fl_reader.read()).split())
-    int_check = re.compile(r'^\-?[1-9][0-9]*$')
-    str_check = re.compile(r'^[a-zA-Z]+$')
-    for val in fl_numsl:
-        if(re.match(str_check, val)):
-            fl_numsl.remove(val)
-        
-        # else:
-        #     if(re.match(int_check,val)):
-        #         fl_numsl.append(int(val))
-            
-    print(fl_numsl)   
-    return fl_numsl
+    fl_mess= list(str(fl_reader.read()).replace("  ", " ").split())
+    nums_list = list()
+
+    for val in fl_mess:
+        try:
+            nums_list.append(int(val))
+        except:
+            try:
+                nums_list.append(float(val))
+            except:
+                continue
+    print(nums_list)
 
 read_numbers("ex1_data.txt")
