557abca766ec1bf6c0ca405979e15ecd9aa75ca1
Qtakmalay <azat.v.r.15@gmail.com>
2022-12-07 04:40:34 +0100

    completed ex3

--- 6thUnit/ex3.py
+++ 6thUnit/ex3.py
@@ -1,21 +1,24 @@
 import os, glob
 
-def get_abs_paths(root_path: str, ext_filter: str = None):
+def get_abs_paths(root_path: str, ext_filter: str = None) -> list:
     try:
-        if(ext_filter == None):
-            list_dirs = glob.glob(os.path.join(root_path, "**"), recursive=True)
-            list_dirs.sort()
-            for val in list_dirs:
-                print(val)
-        else:
-            list_dirs = glob.glob(os.path.join(root_path, "**", "*.py"), recursive=True)
-            list_dirs.sort()
-            for val in list_dirs:
-                print(val)
-        if root_path.isdir():
+        if not(os.path.isdir(root_path)):
             raise ValueError
+        if(ext_filter == None):
+            list_dirs = glob.glob(os.path.join(root_path, "**", "*"), recursive=True)
+            list_dirs.sort()
+            if len(list_dirs) == 0:
+                return list()
+            return list_dirs
+        else:
+            list_dirs = glob.glob(os.path.join(root_path, "**", "*"+ext_filter), recursive=True)
+            list_dirs.sort()
+            if len(list_dirs) == 0:
+                return list()
+            if ext_filter.find('.') == -1:
+                raise ValueError
+            return list_dirs
     except ValueError as ex:
         print(f"We caught the exception '{ex}'")
             
 
-get_abs_paths("C:\\Users\\azatv\\")
