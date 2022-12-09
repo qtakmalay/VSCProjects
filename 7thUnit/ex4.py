925f8c83eb8c7d4dc0ec412a4df12fd7856b79b4
Qtakmalay <azat.v.r.15@gmail.com>
2022-12-09 01:48:29 +0100

    clean up

--- 7thUnit/ex4.py
+++
@@ -1,6 +0,0 @@
-def flatten(nested : list):
-    if not nested:
-        return nested
-    if isinstance(nested[0], list):
-        return flatten(*nested[:1]) + flatten(nested[1:])
-    return nested[:1] + flatten(nested[1:])
