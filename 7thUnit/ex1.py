201c07fe262c18ff8565c0b04386834ad94bf0ce
Qtakmalay <azat.v.r.15@gmail.com>
2022-12-09 01:47:53 +0100

    finished ex1

--- 7thUnit/ex1.py
+++ 7thUnit/ex1.py
@@ -1,8 +1,20 @@
-def fib(n: int) -> int:
-    if n == 1:
-        return n
-    if n == 0:
-        return 0
-    if n < 0:
-        return -1
-    return fib(n-1) + fib(n-2)
+import math
+class  Complex:
+    # real, imaginary = self.real, None
+    def __init__(self, real: float, imaginary: float):
+        self.real = real
+        self.imaginary = imaginary
+
+    def print(self):
+        sign=""
+        if self.imaginary > 0: sign = "+"
+        print(f"{self.real}{sign}{self.imaginary}i")
+
+    def abs(self) -> float:
+        return math.sqrt(pow(self.real, 2) + pow(self.imaginary, 2))
+
+c1 = Complex(1.2, -5.4)
+c1.print()
+c2 = Complex(3.0, 4.0)
+c2.print()
+print(c2.abs())
