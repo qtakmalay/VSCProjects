5a684ca7d28581e8def4fb170e4c85c6a29f0419
Qtakmalay <azat.v.r.15@gmail.com>
2023-01-09 00:18:11 +0100

    Finished init and close methods in ex2

--- 8thUnit/ex2.py
+++ 8thUnit/ex2.py
@@ -1,23 +1,28 @@
-import math
+import math, os
 class Reader:
     def __init__(self, path: str):
-        raise NotImplementedError
+        self.path = path
+        if os.path.isfile(path):
+            self.file = open(path, "rb")
+        else: raise ValueError
+    
     def close(self):
-        raise NotImplementedError
+        self.file.close()
     def __len__(self):
         raise NotImplementedError
     def __getitem__(self, key):
         raise NotImplementedError
 
-r = Reader("ex2_data.txt")
-print(r[0])
-print(r[1])
-print(r[-1])
-try:
-    r["hi"]
-except TypeError as e:
-    print(f"{type(e).__name__}: {e}")
-try:
-    r[100]
-except IndexError as e:
-    print(f"{type(e).__name__}: {e}")
+r = Reader("C:\\Users\\azatv\\VSCProjects\\8thUnit\\ex2_data.txt")
+r.file.close()
+# print(r[0])
+# print(r[1])
+# print(r[-1])
+# try:
+#     r["hi"]
+# except TypeError as e:
+#     print(f"{type(e).__name__}: {e}")
+# try:
+#     r[100]
+# except IndexError as e:
+#     print(f"{type(e).__name__}: {e}")
