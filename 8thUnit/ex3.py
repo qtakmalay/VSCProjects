b6e5eba70f2e8ee52657abe876affcd153d9d77d
Qtakmalay <azat.v.r.15@gmail.com>
2023-01-10 21:56:10 +0100

    Finished ex3

--- 8thUnit/ex3.py
+++ 8thUnit/ex3.py
@@ -2,9 +2,22 @@ import math
 
 class Aggregator:
     def __init__(self, agg_type: type, ignore_errors: bool = True):
-        raise NotImplementedError
-    def __call__(self, *args: Any, **kwds: Any) -> Any:
-        raise NotImplementedError
+        self.agg_type = agg_type
+        self.ignore_errors = ignore_errors
+        self.sum = None
+    def __call__(self, *args):
+        if args.__len__() == 0: return self.sum
+        for arg in args:
+            if isinstance(arg, self.agg_type):
+                if self.sum is None:
+                    self.sum = arg
+                else:
+                    self.sum += arg
+            if not isinstance(arg, self.agg_type) and not self.ignore_errors:
+                raise TypeError(f"Args must be {self.agg_type.__name__}, not {type(arg).__name__}")
+        return self.sum
+            
+
 
 int_agg = Aggregator(agg_type=int)
 int_agg(1, 2, 3)
