1ff9f6c7b898cc1fed3d453d971095104d5c6909
Qtakmalay <azat.v.r.15@gmail.com>
2023-01-10 14:32:48 +0100

    Finished ex2

--- 8thUnit/ex2.py
+++ 8thUnit/ex2.py
@@ -3,26 +3,35 @@ class Reader:
     def __init__(self, path: str):
         self.path = path
         if os.path.isfile(path):
-            self.file = open(path, "rb")
+            self.fh = open(path, "rb")
         else: raise ValueError
     
     def close(self):
-        self.file.close()
+        self.fh.close()
     def __len__(self):
-        raise NotImplementedError
+        return os.path.getsize(self.path)
     def __getitem__(self, key):
-        raise NotImplementedError
+        if isinstance(key, int):
+            if key < 0:
+                key += self.__len__()
+            if key<=self.__len__() and key>=0:
+                self.fh.seek(key)
+                return self.fh.read(1) 
+            else:
+                raise IndexError("Point index out of range")
+        raise TypeError(f"Point indices must be integers, not {type(key).__name__}")
 
 r = Reader("C:\\Users\\azatv\\VSCProjects\\8thUnit\\ex2_data.txt")
-r.file.close()
-# print(r[0])
-# print(r[1])
-# print(r[-1])
-# try:
-#     r["hi"]
-# except TypeError as e:
-#     print(f"{type(e).__name__}: {e}")
-# try:
-#     r[100]
-# except IndexError as e:
-#     print(f"{type(e).__name__}: {e}")
+print(os.path.getsize(r.path))
+# print(r.__len__)
+print(r[0])
+print(r[1])
+print(r[-1])
+try:
+    r["hi"]
+except TypeError as e:
+    print(f"{type(e).__name__}: {e}")
+try:
+    r[100]
+except IndexError as e:
+    print(f"{type(e).__name__}: {e}")
