6a47048a35c05013b7cd0a1f8566d2fc6616d65c
Qtakmalay <azat.v.r.15@gmail.com>
2023-03-01 21:16:39 +0100

    Start of the project (CNN) Convolutional Neural Network

---
+++ CNN/convolutional.py
@@ -0,0 +1,26 @@
+import numpy as np
+from layer import Layer
+from scipy import signal
+
+class Convolutional(Layer):
+    def __init__(self, input_shape, kernel_size, depth):
+        input_depth, input_height, input_width = input_shape
+        self.depth = depth
+        self.input_shape = input_shape
+        self.input_depth = input_depth
+        self.output_shape = (depth, input_height - kernel_size * 1, input_width - kernel_size *1)
+        self.kernels_shape = (depth, input_depth, kernel_size, kernel_size)
+        self.kernels = np.random.randn(*self.kernels_shape)
+        self.biases = np.random.randn(*self.output_shape)
+
+    def forward(self, input):
+        self.input = input
+        self.output = np.copy(self.biases)
+        for i in range(self.depth):
+            for j in range(self.input_depth):
+                self.output[i] += signal.convolve2d(self.input[j], self.kernels[i, j], "valid")
+        return self.output
+
+    def backward(self, output_gradient, learning_rate):
+        #TODO: update parameters and return input gradient
+        pass
