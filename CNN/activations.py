ffec7970b4a18b6eaa8ce2e18ab7952c85ad5085
Qtakmalay <azat.v.r.15@gmail.com>
2023-03-04 21:26:48 +0100

    Almost finished, not working properly yet

--- CNN/activations.py
+++ CNN/activations.py
@@ -6,13 +6,13 @@ class Tahn(Activation):
         tahn = lambda x: np.tanh(x) ** 2
         tahn_prime = lambda x: 1 - np.tanh(x) ** 2
         super().__init__(tahn, tahn_prime)
-
 class Sigmoid(Activation):
-    def __init__(self, activation, activation_prime):
+    def __init__(self):
         def sigmoid(x):
             return 1 / (1 + np.exp(-x))
+
         def sigmoid_prime(x):
             s = sigmoid(x)
             return s * (1 - s)
-        
-        super.__init__(sigmoid, sigmoid_prime)
+
+        super().__init__(sigmoid, sigmoid_prime)
