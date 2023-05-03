73795ce42278bb787dfcbcc3c7b2242adbb4e50c
Qtakmalay <azat.v.r.15@gmail.com>
2023-05-03 08:44:21 +0200

    Finished first ex

--- Second Python/U5/a3_ex1.py
+++ Second Python/U5/a3_ex1.py
@@ -40,48 +40,36 @@ class RandomImagePixelationDataset(Dataset):
         self.height_range = height_range
         self.size_range = size_range
         self.dtype = dtype
-        self.images = self.load_images()
-    def load_images(self):
-        # Implement a function to load and preprocess the images
-        return sorted(glob.glob(os.path.join(self.image_dir, "**", "*.jpg"), recursive=True))
-
+        self.images = sorted(glob.glob(os.path.join(self.image_dir, "**", "*.jpg"), recursive=True))
+    
     def __len__(self):
         return len(self.images)
-
+    
     def __getitem__(self, idx):
+        with Image.open(self.images[idx]) as im:
+            image = np.array(im, dtype=self.dtype if self.dtype is not None else None)
 
-        with Image.open(self.images[idx]) as im:  # This returns a PIL image
-            image = np.array(im,dtype=self.dtype if self.dtype is not None else None)  
         image = to_grayscale(image)
         rng = np.random.default_rng(seed=idx)
         width = int(rng.random() * (self.width_range[1] - self.width_range[0]) + self.width_range[0])
-        print(width)
         height = int(rng.random() * (self.height_range[1] - self.height_range[0]) + self.height_range[0])
-        print(height)
-        x = int(rng.uniform(low=0, high = self.width_range[1] - width))
-        print(x)
-        y = int(rng.uniform(low=0, high = self.width_range[1] - width))
-        print(y)
+
+        x = int(rng.uniform(low=0, high=max(image.shape[1] - width, 0)))
+        y = int(rng.uniform(low=0, high=max(image.shape[0] - height, 0)))
+
         size = int(rng.random() * (self.size_range[1] - self.size_range[0]) + self.size_range[0])
-        print(size)
-        image = im_proc_f.prepare_image(image, x, y, width, height, size)
-        return image
+
+        pixelated_image, known_array, target_array = im_proc_f.prepare_image(image, x, y, width, height, size)
+
+        return pixelated_image, known_array, target_array, self.images[idx]
+
     
 import matplotlib.pyplot as plt
+
 ds = RandomImagePixelationDataset(
-r"C:\Users\azatv\VSCProjects\Second Python\U5\05_images",
-width_range=(50, 300),
-height_range=(50, 300),
-size_range=(10, 50)
+    r"C:\Users\azatv\VSCProjects\Second Python\U5\05_images",
+    width_range=(10, 60),
+    height_range=(10, 60),
+    size_range=(2, 10)
 )
-for pixelated_image, known_array, target_array, image_file in ds:
-    fig, axes = plt.subplots(ncols=3)
-    axes[0].imshow(pixelated_image[0], cmap="gray", vmin=0, vmax=255)
-    axes[0].set_title("pixelated_image")
-    axes[1].imshow(known_array[0], cmap="gray", vmin=0, vmax=1)
-    axes[1].set_title("known_array")
-    axes[2].imshow(target_array[0], cmap="gray", vmin=0, vmax=255)
-    axes[2].set_title("target_array")
-    fig.suptitle(image_file)
-    fig.tight_layout()
-plt.show()
+
