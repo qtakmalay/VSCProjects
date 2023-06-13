fe800a5230962036789909307e2acfd31174afbc
qtakmalay <azat.v.r.15@gmail.com>
2023-06-13 23:47:23 +0200

    finished ex2

--- Second Python/Assign6/a6_ex2.py
+++ Second Python/Assign6/a6_ex2.py
@@ -1,6 +1,6 @@
 
 import torch
-from typing import Sequence, Union
+from typing import Sequence, Union, Tuple
 import os, glob
 from PIL import Image
 import numpy as np
@@ -11,18 +11,22 @@ class ImageDataset(torch.utils.data.Dataset):
       self.list_files = sorted(glob.glob(os.path.join(image_dir, "**", "*.jpg"), recursive=True))
    def __getitem__(self, index: int):
       with Image.open(self.list_files[index]) as im:
-         return (np.array(im), index)
+         return (torchvision.transforms.ToTensor()(im), index)
    def __len__(self):
       return len(self.images)
    
 class TransformedImageDataset(torch.utils.data.Dataset):
-   def __init__(self, dataset: ImageDataset, image_size: Union[int, Sequence[int, int]]):
+   def __init__(self, dataset: ImageDataset, image_size: Union[int, Tuple[int]]):
       self.dataset = dataset
-      self.tramsform = torchvision.transforms.Resize(size=image_size)
+      self.image_size = image_size
    def __getitem__(self, index: int):
-      
+      pil_to_tensor = torchvision.transforms.functional.to_tensor
+      tensor_image = pil_to_tensor(random_augmented_image(self.dataset[index], 
+                                           self.image_size, index))
+      return (tensor_image, index)
+         
    def __len__(self):
-      pass
+      return len(self.dataset)
    
 if __name__ == "__main__":
     from matplotlib import pyplot as plt
@@ -32,8 +36,8 @@ if __name__ == "__main__":
         fig, axes = plt.subplots(1, 2)
         axes[0].imshow(original_img)
         axes[0].set_title("Original image")
-        axes[1].imshow(transforms.functional.to_pil_image(transformed_img))
+        axes[1].imshow(torchvision.transforms.functional.to_pil_image(transformed_img))
         axes[1].set_title("Transformed image")
         fig.suptitle(f"Image {index}")
         fig.tight_layout()
-    plt.show()
+        plt.show()
