65ae22b70a4da06609b056f8852936aa5e0cbee2
qtakmalay <azat.v.r.15@gmail.com>
2023-06-14 00:46:46 +0200

    fixed ex2

--- Second Python/Assign6/a6_ex2.py
+++ Second Python/Assign6/a6_ex2.py
@@ -10,8 +10,7 @@ class ImageDataset(torch.utils.data.Dataset):
    def __init__(self, image_dir):
       self.list_files = sorted(glob.glob(os.path.join(image_dir, "**", "*.jpg"), recursive=True))
    def __getitem__(self, index: int):
-      with Image.open(self.list_files[index]) as im:
-         return (torchvision.transforms.ToTensor()(im), index)
+      return (Image.open(self.list_files[index]), index)
    def __len__(self):
       return len(self.images)
    
@@ -20,9 +19,9 @@ class TransformedImageDataset(torch.utils.data.Dataset):
       self.dataset = dataset
       self.image_size = image_size
    def __getitem__(self, index: int):
-      pil_to_tensor = torchvision.transforms.functional.to_tensor
-      tensor_image = pil_to_tensor(random_augmented_image(self.dataset[index], 
-                                           self.image_size, index))
+      image_tensor, _ = self.dataset[index]
+      tensor_image = random_augmented_image(image_tensor, 
+                                           self.image_size, index)
       return (tensor_image, index)
          
    def __len__(self):
@@ -30,7 +29,7 @@ class TransformedImageDataset(torch.utils.data.Dataset):
    
 if __name__ == "__main__":
     from matplotlib import pyplot as plt
-    imgs = ImageDataset(image_dir="images")
+    imgs = ImageDataset(image_dir="C:\\Users\\azatv\\VSCode\\VSCProjects\\Second Python\\Assign6")
     transformed_imgs = TransformedImageDataset(imgs, image_size=300)
     for (original_img, index), (transformed_img, _) in zip(imgs, transformed_imgs):
         fig, axes = plt.subplots(1, 2)
